e310bfd4c28041a5ac3aadc2378e1595cdbcb851
Drew Chase <dcmanproductions@gmail.com>
2023-09-27 05:36:21 -0400

    added rust docs

--- src/option.rs
+++ src/option.rs
@@ -1,9 +1,65 @@
-#[derive(Clone)]
+//! # Option
+//!
+//! The `Option` struct represents a command-line option that can be used with an application.
+//!
+//! ## Usage
+//!
+//! ```rust
+//! use crate::option::Option;
+//!
+//! // Create an Option instance
+//! let option = Option::new("v".to_string(), "version".to_string(), false, false, "Prints version information".to_string());
+//!
+//! // Access the properties of the Option
+//! println!("Short Name: {}", option.short_name);
+//! println!("Long Name: {}", option.long_name);
+//! println!("Required: {}", option.required);
+//! println!("Has Argument: {}", option.has_argument);
+//! println!("Description: {}", option.description);
+//! ```
+//!
+//! ## Examples
+//!
+//! ### Creating an Option Instance
+//!
+//! ```rust
+//! use crate::option::Option;
+//!
+//! // Create a new Option instance
+//! let option = Option::new("v".to_string(), "version".to_string(), false, false, "Prints version information".to_string());
+//! ```
+//!
+//! ### Getting Option Properties
+//!
+//! ```rust
+//! use crate::option::Option;
+//!
+//! let option = Option::new("v".to_string(), "version".to_string(), false, false, "Prints version information".to_string());
+//!
+//! println!("Short Name: {}", option.short_name);
+//! println!("Long Name: {}", option.long_name);
+//! println!("Required: {}", option.required);
+//! println!("Has Argument: {}", option.has_argument);
+//! println!("Description: {}", option.description);
+//! ```
+//!
+//! ## License
+//!
+//! This code is licensed under the MIT License.
+//!
+//! ## Disclaimer
+//!
+//! This code is provided as-is, and the authors make no warranties regarding its correctness or fitness for any purpose.
+//!
+//! Please feel free to report issues or submit pull requests to improve this code.
+//!
+
 /// The Option struct represents a command line option.
+#[derive(Clone)]
 pub struct Option {
-    /// The short name of the option, e.g. "h" for "-h".
+    /// The short name of the option, e.g., "h" for "-h".
     pub short_name: String,
-    /// The long name of the option, e.g. "help" for "--help".
+    /// The long name of the option, e.g., "help" for "--help".
     pub long_name: String,
     /// Whether the option is required or not.
     pub required: bool,
@@ -17,11 +73,32 @@ pub struct Option {
 
 /// The implementation of the Option struct.
 impl Option {
+    /// Creates a new Option instance with the specified properties.
+    ///
+    /// # Arguments
+    ///
+    /// * `short_name` - A string representing the short name of the option.
+    /// * `long_name` - A string representing the long name of the option.
+    /// * `required` - A boolean indicating whether the option is required.
+    /// * `has_argument` - A boolean indicating whether the option has an argument.
+    /// * `description` - A string representing the description of the option.
+    ///
+    /// # Returns
+    ///
+    /// A new `Option` instance.
     pub fn new(short_name: String, long_name: String, required: bool, has_argument: bool, description: String) -> Self {
         Self { short_name, long_name, required, has_argument, description, argument: String::new() }
     }
-    /// Returns the string representation of the Option struct.
+
+    /// Returns a string representation of the Option struct.
+    ///
+    /// # Returns
+    ///
+    /// A formatted string containing the option's properties.
     pub fn to_string(&self) -> String {
-        return format!("short:{}, long:{}, required:{}, has argument:{}, description:{}", self.short_name, self.long_name, self.required, self.has_argument, self.description);
+        format!(
+            "short:{}, long:{}, required:{}, has argument:{}, description:{}",
+            self.short_name, self.long_name, self.required, self.has_argument, self.description
+        )
     }
 }
--- src/options_manager.rs
+++ src/options_manager.rs
@@ -1,3 +1,95 @@
+//! # OptionsManager
+//!
+//! The `OptionsManager` struct represents a command line option manager.
+//!
+//! ## Usage
+//!
+//! ```rust
+//! use std::collections::HashSet;
+//! use crate::option::Option;
+//!
+//! let mut options_manager = OptionsManager::new("MyApp", Vec::new());
+//! let args = vec!["-h", "-o", "output.txt"];
+//!
+//! if let Ok(present_options) = options_manager.parse_options(args) {
+//!     if options_manager.is_present("h") {
+//!         options_manager.print_help();
+//!     } else {
+//!         for option in &options_manager.options {
+//!             // Check if the option is present
+//!             if options_manager.is_present(&option.short_name) {
+//!                 let argument = options_manager.argument(&option.short_name);
+//!                 // Process the option and its argument
+//!                 println!("Option: -{}, Argument: {}", option.short_name, argument);
+//!             }
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! ## Examples
+//!
+//! ### Creating an `OptionsManager` Instance
+//!
+//! ```rust
+//! use rclip::{Option, OptionsManager};
+//!
+//! // Create a new OptionsManager instance with default "help" option
+//! let options = vec![
+//!     Option::new("v".to_string(), "version".to_string(), false, false, "Prints version information".to_string()),
+//!     Option::new("f".to_string(), "file".to_string(), true, true, "The file to read".to_string()),
+//!     Option::new("o".to_string(), "output".to_string(), false, true, "The file to write to".to_string()),
+//! ];
+//!
+//! let mut options_manager = OptionsManager::new("MyApp", options);
+//! ```
+//!
+//! ### Parsing Command-Line Options
+//!
+//! ```rust
+//! let args = vec!["-h", "-o", "output.txt"];
+//! if let Ok(present_options) = options_manager.parse_options(args) {
+//!     // Check if the "help" option is present
+//!     if options_manager.is_present("h") {
+//!         options_manager.print_help();
+//!     } else {
+//!         for option in &options_manager.options {
+//!             // Check if the option is present
+//!             if options_manager.is_present(&option.short_name) {
+//!                 let argument = options_manager.argument(&option.short_name);
+//!                 // Process the option and its argument
+//!                 println!("Option: -{}, Argument: {}", option.short_name, argument);
+//!             }
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! ### Checking for Required Options
+//!
+//! ```rust
+//! let args = vec!["-f", "input.txt"];
+//! if let Ok(present_options) = options_manager.parse_options(args) {
+//!     for option in &options_manager.options {
+//!         // Check if a required option is missing
+//!         if option.required && !options_manager.is_present(&option.short_name) {
+//!             eprintln!("Required option -{} or --{} is missing.", option.short_name, option.long_name);
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! ## License
+//!
+//! This code is licensed under the MIT License.
+//!
+//! ## Disclaimer
+//!
+//! This code is provided as-is, and the authors make no warranties regarding its correctness or fitness for any purpose.
+//!
+//! Please feel free to report issues or submit pull requests to improve this code.
+//!
+
 use std::collections::HashSet;
 use crate::option::Option;
 
@@ -11,12 +103,30 @@ pub struct OptionsManager {
 /// `OptionsManager` implementation.
 impl OptionsManager {
     /// Instantiates a new `OptionsManager` instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `context` - A string representing the application context.
+    /// * `options` - A vector of `Option` objects representing the available command-line options.
+    ///
+    /// # Returns
+    ///
+    /// A new `OptionsManager` instance.
     pub fn new(context: &str, mut options: Vec<Option>) -> Self {
         options.push(Option::new("h".to_string(), "help".to_string(), false, false, "Displays the help screen".to_string()));
         Self { context: context.to_string(), options, present_options: Vec::new() }
     }
 
     /// Parses the command line options and returns option that are present.
+    ///
+    /// # Arguments
+    ///
+    /// * `args` - A vector of strings representing the command-line arguments.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing a vector of `Option` objects representing the parsed options if successful,
+    /// or an error message as a string if parsing fails.
     pub fn parse_options(&mut self, args: Vec<String>) -> Result<Vec<Option>, String> {
         let mut present_options: Vec<Option> = Vec::new();
         let mut option_set: HashSet<&str> = HashSet::new();
@@ -77,11 +187,27 @@ impl OptionsManager {
     }
 
     /// Checks if the given option is present in the parsed options list.
+    ///
+    /// # Arguments
+    ///
+    /// * `option` - A string representing the option to check.
+    ///
+    /// # Returns
+    ///
+    /// `true` if the option is present; otherwise, `false`.
     pub fn is_present(&self, option: &str) -> bool {
         return self.present_options.iter().any(|o| o.short_name == option || o.long_name == option);
     }
 
     /// Returns the argument of the given option.
+    ///
+    /// # Arguments
+    ///
+    /// * `option` - A string representing the option to retrieve the argument for.
+    ///
+    /// # Returns
+    ///
+    /// The argument of the option as a string.
     pub fn argument(&self, option: &str) -> String {
         let mut argument = String::new();
         if let Some(option) = self.present_options.iter().find(|o| o.has_argument && (o.short_name == option || o.long_name == option)) {
